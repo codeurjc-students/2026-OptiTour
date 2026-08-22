eaa3f2ed56a429ac827dae3f54fc1df41f0bec4f
MarcosHM851 <m.hernandezm.2023@alumnos.urjc.es>
2026-08-22 13:13:37 +0200

    Initialize Spring Boot backend project

---
+++ backend/optitour/src/main/java/es/urjc/tfg/optitour/OptitourApplication.java
@@ -0,0 +1,13 @@
+package es.urjc.tfg.optitour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptitourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptitourApplication.class, args);
+	}
+
+}
