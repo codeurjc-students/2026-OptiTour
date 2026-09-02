43473aed7ab43d0329f5fa9c5c7cb951e07003a7
MarcosHM851 <m.hernandezm.2023@alumnos.urjc.es>
2026-09-02 14:24:46 +0200

    Add complete workflow & solve maven build problem

--- backend/optitour/src/main/java/es/urjc/tfg/optitour/OptitourApplication.java
+++ backend/optitour/src/main/java/es/urjc/tfg/optitour/OptitourApplication.java
@@ -2,8 +2,12 @@ package es.urjc.tfg.optitour;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import es.urjc.tfg.optitour.mapper.TourMapperImpl;
 
 @SpringBootApplication
+@Import(TourMapperImpl.class)
 public class OptitourApplication {
 
 	public static void main(String[] args) {
