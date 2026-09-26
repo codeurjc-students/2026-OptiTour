b91641129c73b1984c20612043bba1561395343b
MarcosHM851 <m.hernandezm.2023@alumnos.urjc.es>
2026-09-26 12:17:36 +0200

    Add pagable endpoint

--- backend/optitour/src/main/java/es/urjc/tfg/optitour/OptitourApplication.java
+++ backend/optitour/src/main/java/es/urjc/tfg/optitour/OptitourApplication.java
@@ -2,8 +2,10 @@ package es.urjc.tfg.optitour;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class OptitourApplication {
 
 	public static void main(String[] args) {
